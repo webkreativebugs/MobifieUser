7c8dad8af8c088ad4ac358b9977b37a87bf7b304
codinggiant12 <anubhavchaurasiya278@gmail.com>
2025-08-07 14:07:29 +0530

    signin ui

--- src/components/common_component/AuthRight.tsx
+++ src/components/common_component/AuthRight.tsx
@@ -1,7 +1,16 @@
 function AuthRight() {
   return (
     <div className="text-black h-screen w-svw  bg-image">
-      <h1></h1>
+      <div className="pt-10 pl-14 ">
+        <p className="text-2xl   text-green-950">
+          Welcome to{" "}
+          <h1 className="text-5xl  font-semibold  scale-y-100">Mobifie</h1>
+        </p>
+        <p className="w-2/5  mt-5 text-lg  font-lg">
+          From concept to launch, Mobifie gives you the tools to build, manage,
+          and grow your product — all in one seamless platform.
+        </p>
+      </div>
     </div>
   );
 }
