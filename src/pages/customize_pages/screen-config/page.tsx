c6bfd0767788bbd1acb55e1e565669112644710d
CS-Er-prakhar <web.kreativebugs@gmail.com>
2025-08-16 11:23:24 +0530

    dragdroptrial

--- src/pages/customize_pages/screen-config/page.tsx
+++ src/pages/customize_pages/screen-config/page.tsx
@@ -1,10 +1,15 @@
-
-
+import AppConfigMask from "../../../components/module/project_component/ConfigComponents/app/AppConfigMask"
+import DragAndDrop from "../../../components/module/project_component/ConfigComponents/app/DragAndDrop"
+// import { ConfigTypeEnums } from "../../../../enum/DashboardLinks"
 const page = () => {
   return (
-    <div>
-      
-    </div>
+
+       <AppConfigMask displayName={"Screen"}>
+      <>
+        <DragAndDrop/>
+      </>
+    </AppConfigMask>
+    
   )
 }
 
