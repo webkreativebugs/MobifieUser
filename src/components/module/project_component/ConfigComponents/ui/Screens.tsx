3c9c2a785a5c7285dd6186822b55917f0c8c86bb
CS-Er-prakhar <web.kreativebugs@gmail.com>
2025-09-18 13:35:06 +0530

    changes

--- src/components/module/project_component/ConfigComponents/ui/Screens.tsx
+++ src/components/module/project_component/ConfigComponents/ui/Screens.tsx
@@ -1,99 +1,47 @@
-import React, { useState } from "react";
-import UiCOmponent from "../../../../../data/CustomizeData/UiDropdown.json";
-<<<<<<< HEAD
-import COmponent from "../../../../../data/CustomizeData/Component.json";
-import type { UiConfigSidebarProps } from "./Ui";
-=======
-import headerconfig from "../../../../../data/CustomizeData/HeaderConfig.json";
-import bottomconfig from "../../../../../data/CustomizeData/BottomTabConfig.json";
-import type { UiConfigSidebarProps } from "./Ui";
-import Header from "./customComponent/header/Header1";
-import Header1 from "./customComponent/header/Header1";
-import BottomTab1 from "./customComponent/bottomTab/BottomTab1";
-import PageRenderer from "../../../../uiComponents/PageRerender";
-import { componentsMap } from "../../../../../components/uiComponents/mapComponents";
->>>>>>> 20116fd45904cfe4d9f61e041965fdcb2fe851c6
+import React, { useEffect, useState, Dispatch, SetStateAction } from "react";
+import { ScreenConfigInterface } from "../../../../../data/interface/data.interface";
+import { useSaveChanges } from "../../../../../context/ui_context/SaveChanges";
+type UiConfigSidebarProps = {
+  screenConfig: ScreenConfigInterface;
+  setscreenConfig: Dispatch<SetStateAction<ScreenConfigInterface>>;
+};
 
-interface Section {
-  type: string;
-  url: string;
-}
+function Screens({ screenConfig, setscreenConfig }: UiConfigSidebarProps) {
+  const { isActive, setIsActive } = useSaveChanges();
+  const addNewDesign = (newUrl: string) => {
+    setscreenConfig((prev) => ({
+      ...prev,
+      current_confi: {
+        ...prev.current_confi,
+        screen: {
+          ...prev.current_confi.screen,
+          image: newUrl,
+        },
+      },
+    }));
+    setIsActive(true);
+  };
 
-interface ScreenData {
-  name: string;
-  type: string;
-  header: Section;
-  footer: Section;
-  main: string[];
-}
-
-function Screens({ data, handleUI }: UiConfigSidebarProps | any) {
   return (
-<<<<<<< HEAD
-    <div className="columns-3 gap-6  px-8 mt-16 ">
-      {/* <h1 className="text-black">{element}</h1> */}
-      {data?.designs?.map((item: string, idx: number) => (
-        <div
-          key={idx}
-          className="mb-4 break-inside-avoid border p-2 pb-4 w-[15rem] max-h-[27rem] overflow-hidden rounded-lg bg-white shadow"
-        >
-          <button
-            onClick={() => handleUI(item, data.name)}
-            className="w-full block"
-          >
-            <img src={item} alt="" className="w-full h-auto object-contain" />
-          </button>
-        </div>
-      ))}
-      {/* <div dangerouslySetInnerHTML={{ __html: COmponent[0].value }} /> */}
-    </div>
-=======
     <>
-      <div className="columns-3 gap-6 px-8 mt-16">
-        {data?.designs?.map((item: string, idx: number) =>
-          data?.name === "Header" || data?.name === "Bottom Tab" ? (
-            <div
-              key={idx}
-              className="mb-4 break-inside-avoid border p-2 pb-4 w-[18rem] max-h-[27rem] overflow-hidden rounded-lg bg-white shadow"
+      <div className="columns-3 gap-4 px-2 mt-16">
+        {screenConfig.Screen.map((screenItem, idx) => (
+          <div
+            key={idx}
+            className="mb-4 break-inside-avoid border p-2 pb-4 w-[15rem] max-h-[27rem] overflow-hidden rounded-lg bg-white shadow"
+          >
+            <button
+              onClick={() => addNewDesign(screenItem.image)}
+              className="w-full block"
             >
-              <button className="w-full block">
-                {data?.name === "Header" ? (
-                  <>
-                    {headerconfig.map((block, i) => {
-                      const Component = componentsMap[block.component]; // name se component uthao
-                      if (!Component) return null;
-                      return (
-                        <button
-                        // onClick={() => handleUI(item, data.name)}
-                        >
-                          <Component key={i} {...block.props} />
-                        </button>
-                      );
-                    })}
-                  </>
-                ) : (
-                  <PageRenderer config={bottomconfig} />
-                )}
-              </button>
-            </div>
-          ) : (
-            <div
-              key={idx}
-              className="mb-4 break-inside-avoid border p-2 pb-4 w-[18rem] max-h-[27rem] overflow-hidden rounded-lg bg-white shadow"
-            >
-              <button
-                onClick={() => handleUI(item, data.name)}
-                className="w-full block"
-              >
-                <img
-                  src={item}
-                  alt=""
-                  className="w-full h-auto object-contain"
-                />
-              </button>
-            </div>
-          )
-        )}
+              <img
+                src={screenItem.image}
+                alt=""
+                className="w-full h-auto object-contain"
+              />
+            </button>
+          </div>
+        ))}
       </div>
     </>
 >>>>>>> 20116fd45904cfe4d9f61e041965fdcb2fe851c6
