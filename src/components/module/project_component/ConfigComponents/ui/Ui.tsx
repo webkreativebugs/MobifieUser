299278b798b0abc2f7e7837079f62578adb6ffdf
CS-Er-prakhar <web.kreativebugs@gmail.com>
2025-10-16 16:54:39 +0530

    client

--- src/components/module/project_component/ConfigComponents/ui/Ui.tsx
+++ src/components/module/project_component/ConfigComponents/ui/Ui.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch, SetStateAction, useEffect } from "react";
+import  { useState, Dispatch, SetStateAction, useEffect } from "react";
 import { ScreenConfigInterface } from "../../../../../data/interface/data.interface";
 
 import Screens from "./Screens";
