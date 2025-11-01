93ccb2a4ebf4affb3b1886bbdbeb956d3637ba4b
CS-Er-prakhar <cs.erprakharsharma@gmail.com>
2025-11-01 20:03:29 +0530

    test deploy 5

--- src/pages/customize_pages/configuration_page/page.tsx
+++ src/pages/customize_pages/configuration_page/page.tsx
@@ -8,19 +8,18 @@ import HeadingMask from "../../../components/common_component/layered_components
 import { Link } from "react-router-dom";
 import { useTabContext } from "../../../context/org_context/TabContext";
 import {
-  useDraftScreenChanges,
   useDraftScreen,
 } from "../../../context/ui_context/DraftScreenContext";
 import { ScreenType } from "../../../../enum/AccessType.enum";
 import { FaSearch } from "react-icons/fa";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
 
 const Page = () => {
-  const { isDraft } = useDraftScreenChanges();
-  const { isEdit, setIsEdit } = useTabContext();
-  const [element, setElement] = useState(ScreenConfigdata[0].key);
+  // const { isDraft } = useDraftScreenChanges();
+  const {  setIsEdit } = useTabContext();
+  const [element] = useState(ScreenConfigdata[0].key);
   const [query, setQuery] = useState({ app: "", screen: "" });
   const [popUp, setPOpUp] = useState(false);
   const { isActive } = useSaveChanges();
@@ -31,6 +30,9 @@ const Page = () => {
       (item) => item.key === element
     ) as ScreenConfigInterface
   );
+
+  console.log(popUp , screenConfig);
+  
 
   const [appDateRange, setAppDateRange] = useState<[Date | null, Date | null]>([
     null,
